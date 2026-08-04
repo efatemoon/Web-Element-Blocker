5c8e4763a2653ed4ea1456f12d5c03e5e79c7024
EFate <3142514149@qq.com>
2026-08-04 17:22:37 +0800

    Use .meta.js for lightweight update checks

--- web-element-blocker.meta.js
+++ web-element-blocker.meta.js
@@ -1,11 +1,15 @@
 // ==UserScript==
-// @name         Web Element Blocker
-// @namespace    https://github.com/efatemoon
-// @version      1.0.0
-// @description  网页元素屏蔽器
-// @author       efatemoon
+// @name         网页元素屏蔽器
+// @namespace    http://tampermonkey.net/
+// @version      0.1.55test
+// @description  集成原生CSS极速注入、Shadow DOM隔离、DOM结构拦截、广告域封杀、正则文本拦截、动态资源域实时拦截、路径模式拦截与规则导入导出。支持积木组合模式、元素层级缩放选择与全局域名黑名单，彻底解决广告刷新复活。
+// @author       EFate
 // @match        *://*/*
-// @grant        none
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @run-at       document-start
+// @license      MIT
 // @downloadURL  https://raw.githubusercontent.com/efatemoon/Web-Element-Blocker/main/web-element-blocker.user.js
-// @updateURL    https://raw.githubusercontent.com/efatemoon/Web-Element-Blocker/main/web-element-blocker.user.js
+// @updateURL    https://raw.githubusercontent.com/efatemoon/Web-Element-Blocker/main/web-element-blocker.meta.js
 // ==/UserScript==
