39013ae4e14f95d02484e3987b3bd665fd57d832
EFate <3142514149@qq.com>
2026-08-04 17:08:07 +0800

    Add .meta.js for lightweight update checks

---
+++ web-element-blocker.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Web Element Blocker
+// @namespace    https://github.com/efatemoon
+// @version      1.0.0
+// @description  网页元素屏蔽器
+// @author       efatemoon
+// @match        *://*/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/efatemoon/Web-Element-Blocker/main/web-element-blocker.user.js
+// @updateURL    https://raw.githubusercontent.com/efatemoon/Web-Element-Blocker/main/web-element-blocker.user.js
+// ==/UserScript==
